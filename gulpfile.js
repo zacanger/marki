409b8bee4da9029a19a579b7807e5324aae33e9e
zacanger <zac@zacanger.com>
2015-11-12 09:49:10 -0700

    this is fully functional... let's see if gulp screws things up.

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,26 @@
 var gulp		 = require('gulp')
 	,	minjs		 = require('gulp-uglify')
 	,	mincss	 = require('gulp-cssmin')
-	,	watcher  = gulp.watch('scripts/**.js', 'styles/**.css', ['minjs', 'mincss']);
+  , concat   = require('gulp-concat')
+//	,	watcher  = gulp.watch('scripts/**.js', 'styles/*.css', ['minjs', 'mincss']);
+	,	watcher  = gulp.watch('scripts/**.js', ['minjs']);
 
 gulp.task('minjs', function(){
-	return gulp.src('scripts/*.js', 'scripts/*/*.js')
-		.pipe(uglify())
+	return gulp.src('scripts/**.js')
+		.pipe(concat('min.js'))
+        .pipe(minjs())
 		.pipe(gulp.dest('dist'));
 });
 
-gulp.task('mincss', function(){
-	return gulp.src('styles/*.css', 'styles/*/*.css')
-	.pipe(cssmin())
-	.pipe(gulp.dest('dist'));
-});
+// gulp.task('mincss', function(){
+//	return gulp.src('styles/*.css', 'styles/*/*.css')
+//	.pipe(cssmin())
+//	.pipe(gulp.dest('dist'));
+//});
 
 watcher.on('change', function(event){
-	console.log('heyo, ur' + event.patch + 'be all ' + event.type + ' at ' + new Date() + '... so i gon run dem tasks fuh you.');
+	console.log('heyo, ur ' + event.patch + ' be all ' + event.type + ' at ' + new Date() + '... so i gon run dem tasks fuh you.');
 });
 
-gulp.task('default', ['minjs', 'mincss']);
+gulp.task('default', ['minjs']);
 
