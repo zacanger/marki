9464157ee302a1c989ebcc734c9090bde5db3499
zacanger <zac@zacanger.com>
2016-02-23 17:08:09 -0700

    foo

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,35 @@
-var gulp = require('gulp')
-  , connect = require('gulp-connect')
+var gulp      = require('gulp')
+  , connect   = require('gulp-connect')
+  , watch     = require('gulp-watch')
+  , concat    = require('gulp-concat')
+  , uglify    = require('gulp-uglify')
+  , uglifycss = require('gulp-uglifycss')
+  , annotate  = require('gulp-ng-annotate')
+  , tasklist  = require('gulp-task-listing')
+
+gulp.task('serve', function(){
+  connect.server({
+    livereload : true
+  , port       : 4444
+  , root       : '.'
+  })
+})
+
+
+gulp.task('js', function(){
+  .src('./**/*.js')
+  .pipe(annotate())
+  .pipe(uglify())
+  .pipe(concat('js.min.js'))
+  .pipe(gulp.dest('./public'))
+})
+
+gulp.task('css', function(){
+  .src('./**/*.css')
+  .pipe(uglifycss())
+  .pipe(concat('css.min.css'))
+  .pipe(gulp.dest('./public'))
+})
 
 gulp.task('serve', function(){
   connect.server({
@@ -9,13 +39,8 @@ gulp.task('serve', function(){
   })
 })
 
-gulp.task('markdown', function(){
-  gulp.src('./markdown/*.md')
+gulp.task('reload', function(){
+  gulp.src('./public/**')
   .pipe(connect.reload())
 })
 
-gulp.task('watch', function(){
-  gulp.watch(['./markdown/*.md'], ['html'])
-})
-
-gulp.task('default', ['watch', 'serve'])
